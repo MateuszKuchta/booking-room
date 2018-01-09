f1b118aefc516014490042ba9fdbe066c9edad50
Mati <mattyekus@gmail.com>
2018-01-09 12:37:48 +0100

    message

--- src/main/webapp/mainplatform/controller/RoomDetails.controller.js
+++ src/main/webapp/mainplatform/controller/RoomDetails.controller.js
@@ -92,7 +92,6 @@ sap.ui.define([
         },
 
         onSettingsPress: function () {
-            console.log(this.createId("settingsButton"));
             if (this.getView().byId("settingsBox").getVisible()) {
                 this.getView().byId("settingsBox").setVisible(false);
             } else {
@@ -386,23 +385,18 @@ sap.ui.define([
                             window.thisRD.getView().byId("plusReservation").setSrc("./resources/images/end.png");
                             jsonStatusModel.oData.status["0"].CurrentOrUse = "In use";
                         } else {
-                            console.log("else");
                             var checkIfLessThan15 = start - actual;
                             if (new Date(checkIfLessThan15).getTime() <= new Date(0).setMinutes(new Date(0).getMinutes() + 15)) {
-                                console.log("ponizej");
                                 jQuery("#__xmlview1--child").addClass("almostFreeRoom");
                                 if (window.thisRD.getView().byId("15minReservation").getVisible())
                                     window.thisRD.onQuickReservation(0);
                                 window.thisRD.getView().byId("plusReservation").setSrc("./resources/images/plus-orange.png");
                             } else {
-                                console.log("bardziej else");
                                 jQuery("#__xmlview1--child").addClass("freeRoom");
                                 if ((window.thisRD.getView().byId("plusReservation").getSrc() !== "./resources/images/plus.png") && (window.thisRD.getView().byId("plusReservation").getSrc() !== "./resources/images/30mins.png")) {
                                     if (window.thisRD.getView().byId("15minReservation").getVisible() || window.thisRD.getView().byId("plusReservation").getSrc() === "./resources/images/end.png")
                                        window.thisRD.onQuickReservation(0);
-                                    console.log("w bardziej");
                                 }
-                                console.log("w");
                             }
                             jsonStatusModel.oData.status["0"].CurrentOrUse = "Available";
                         }
